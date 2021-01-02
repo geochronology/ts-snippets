1a8c3733063a2c60cb7aad407f2f52d2d5626331
geochronology <54387644+geochronology@users.noreply.github.com>
2021-01-02 14:02:11 -0800

    added head component

---
+++ src/components/Head.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {
+  title: string,
+  isActive: boolean
+}
+
+export const Head = ({ title, isActive }: Props) => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
+}
