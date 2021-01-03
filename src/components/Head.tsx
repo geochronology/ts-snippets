78f08ad0e572429789504f30e12e470a59f456b7
geochronology <54387644+geochronology@users.noreply.github.com>
2021-01-03 00:23:54 -0800

    generics

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 type Props = {
   title: string,
-  isActive?: boolean
+  isActive?: boolean,
+  type3?: Array<string>,
+  type4?: string[]
 }
 
 export const Head = ({ title, isActive = true }: Props) => {
