ef40f604fa9c9d7d2640f9eb9b7583edbd1bb697
geochronology <54387644+geochronology@users.noreply.github.com>
2021-01-02 16:45:35 -0800

    added useRef hook

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,7 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 
 export const Input = () => {
   const [name, setName] = useState("") // infers string based on default value
+
+  // read-only ref
+  const ref = useRef<HTMLInputElement>(null)
+
+  if (ref && ref.current) {
+    console.log("ref", ref.current.value)
+  }
 
   return (
     <input value={name} onChange={e => setName(e.target.value)} />
